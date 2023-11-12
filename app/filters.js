b5eb866e7f15e09743e3b135809c5d71d3da546c
Adam Silver <adambsilver@gmail.com>
2023-11-12 12:26:55 +0000

    Show statuses

--- app/filters.js
+++ app/filters.js
@@ -7,4 +7,13 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
-
+addFilter('statusColour', status => {
+  switch(status) {
+    case 'Received':
+      return 'govuk-tag--purple'
+    case 'Rejected':
+      return 'govuk-tag--red'
+    case 'Approved':
+      return 'govuk-tag--green'
+  }
+})
