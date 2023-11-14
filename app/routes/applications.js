221a133b4a5c2f561e2184da7be6be47e309b032
Adam Silver <adambsilver@gmail.com>
2023-11-14 15:10:14 +0000

    Hook up clear filters

--- app/routes/applications.js
+++ app/routes/applications.js
@@ -7,27 +7,51 @@ module.exports = router => {
 
     // ['Received', ...]
     let selectedStatusFilters = _.get(req.session.data.filters, 'statuses')
+    let selectedNumberOfBallsFilters = _.get(req.session.data.filters, 'numberOfBalls')
+
+    let hasFilters = _.get(selectedStatusFilters, 'length') || _.get(selectedNumberOfBallsFilters, 'length')
 
     let selectedFilters = {
       categories: []
     }
 
     // the user has selected a status filter
-    if(_.get(selectedStatusFilters, 'length')) {
+    if(hasFilters) {
       applications = applications.filter(application => {
         let matchesStatus = true
+        let matchesNumberOfBalls = true
 
-        matchesStatus = selectedStatusFilters.includes(application.status);
+        if(_.get(selectedStatusFilters, 'length')) {
+          matchesStatus = selectedStatusFilters.includes(application.status);
+        }
 
-        return matchesStatus
+        if(_.get(selectedNumberOfBallsFilters, 'length')) {
+          matchesNumberOfBalls = selectedNumberOfBallsFilters.includes(application.experience.numberOfBalls);
+        }
+
+        return matchesStatus && matchesNumberOfBalls
       })
+    }
 
+    if(_.get(selectedStatusFilters, 'length')) {
       selectedFilters.categories.push({
         heading: { text: 'Status' },
         items: selectedStatusFilters.map(label => {
           return {
             text: label,
             href: `/applications/remove-status/${label}`
+          }
+        })
+      })
+    }
+
+    if(_.get(selectedNumberOfBallsFilters, 'length')) {
+      selectedFilters.categories.push({
+        heading: { text: 'Number of balls' },
+        items: selectedNumberOfBallsFilters.map(label => {
+          return {
+            text: label,
+            href: `/applications/remove-numberOfBalls/${label}`
           }
         })
       })
@@ -41,6 +65,17 @@ module.exports = router => {
 
   router.get('/applications/remove-status/:status', (req, res) => {
     _.set(req, 'session.data.filters.statuses', _.pull(req.session.data.filters.statuses, req.params.status))
+    res.redirect('/applications')
+  })
+
+  router.get('/applications/remove-numberOfBalls/:numberOfBalls', (req, res) => {
+    _.set(req, 'session.data.filters.numberOfBalls', _.pull(req.session.data.filters.numberOfBalls, req.params.numberOfBalls))
+    res.redirect('/applications')
+  })
+
+  router.get('/applications/clear-filters', (req, res) => {
+    _.set(req, 'session.data.filters.statuses', null)
+    _.set(req, 'session.data.filters.numberOfBalls', null)
     res.redirect('/applications')
   })
 
