4f7c27a7b06aa52da8a606f4045a069d9df1d0cb
Adam Silver <adambsilver@gmail.com>
2023-11-14 14:58:35 +0000

    Add filter for status

--- app/routes/applications.js
+++ app/routes/applications.js
@@ -1,4 +1,48 @@
+const _ = require('lodash')
+
 module.exports = router => {
+
+  router.get('/applications', (req, res) => {
+    let applications = req.session.data.applications
+
+    // ['Received', ...]
+    let selectedStatusFilters = _.get(req.session.data.filters, 'statuses')
+
+    let selectedFilters = {
+      categories: []
+    }
+
+    // the user has selected a status filter
+    if(_.get(selectedStatusFilters, 'length')) {
+      applications = applications.filter(application => {
+        let matchesStatus = true
+
+        matchesStatus = selectedStatusFilters.includes(application.status);
+
+        return matchesStatus
+      })
+
+      selectedFilters.categories.push({
+        heading: { text: 'Status' },
+        items: selectedStatusFilters.map(label => {
+          return {
+            text: label,
+            href: `/applications/remove-status/${label}`
+          }
+        })
+      })
+    }
+
+    res.render('applications/index', {
+      applications,
+      selectedFilters
+    })
+  })
+
+  router.get('/applications/remove-status/:status', (req, res) => {
+    _.set(req, 'session.data.filters.statuses', _.pull(req.session.data.filters.statuses, req.params.status))
+    res.redirect('/applications')
+  })
 
   router.get('/applications/:applicationId', (req, res) => {
     let application = req.session.data.applications.find(application => application.id === req.params.applicationId)
@@ -7,6 +51,8 @@ module.exports = router => {
       application
     })
   })
+
+
 
 
 }
